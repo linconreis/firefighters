2314d9097372d84d189538d4eb20545f6ada8e43
Asus-Linux <asus-linux@localhost.localdomain>
2018-01-30 17:03:00 -0200

    db seed - commandant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+unless Firefighter.exists?
+	Firefighter.create name: "lincon", last_name: "reis", phone: "54996583860", commandant: "true", type_blood: "o+", password: "12345", email: "lincon9891@gmail.com", registration: "101010", birth_date: "13/10/1989", renew_password: "false"
+	Firefighter.create name: "comandante", last_name: "major", phone: "5499999999", commandant: "true", type_blood: "o+", password: "12345", email: "comandante@gmail.com", registration: "151515", birth_date: "10/01/1989", renew_password: "false"
+end
