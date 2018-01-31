738dde940914915a79f45f1b8c8706c30afa1639
Asus-Linux <asus-linux@localhost.localdomain>
2018-01-31 17:32:05 -0200

    encrypt password - SHA:256

--- app/models/firefighter.rb
+++ app/models/firefighter.rb
@@ -1,6 +1,16 @@
 class Firefighter < ApplicationRecord
-
+ require 'digest'
+ before_create :encrypt_password
+ before_update :encrypt_password
+	
 	has_many :pluviometers, dependent: :destroy
 	has_and_belongs_to_many :absences, dependent: :destroy
+
+
+private 
+
+	def encrypt_password
+	  self.password = Digest::SHA256.hexdigest self.password
+	end
 
 end
