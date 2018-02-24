fd1dd2f04b217ba1180fb04aec2920f856849565
Asus-Linux <asus-linux@localhost.localdomain>
2018-02-24 13:44:28 -0300

    add_session_firefighter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def add_firefighter_session(registration)
+  	session[:current_firefighter_registration] = registration
+  end
 end
