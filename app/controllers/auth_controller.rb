6da111be8919cc5c51a79aa9e603fc2f89bf7a0b
Asus-Linux <asus-linux@localhost.localdomain>
2018-02-24 13:46:07 -0300

    auth_controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,4 +1,27 @@
 class AuthController < ApplicationController
+require 'digest'
+
   def index
+
   end
+
+  def sign_in
+  	registration = params[:firefighter][:registration]
+  	password = params[:firefighter][:password]
+  	firefighter = Firefighter.find_by registration: registration
+
+  	if firefighter.present?
+  		if firefighter.password == Digest::SHA256.hexdigest(password)
+  		add_firefighter_session(firefighter.registration)
+  		redirect_to controller: "welcome"
+  		else
+  			flash[:warning] = "Senha inválida!"
+  			render :login
+  		end
+  	else
+  		flash[:warning] = "Usuário não encontrado :("
+  		render :login
+  	end
+  end
+
 end
