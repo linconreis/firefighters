58874d850d48c55685a10b17ff4eea99e1015dd0
Asus-Linux <asus-linux@localhost.localdomain>
2018-02-24 14:59:11 -0300

    add helpers application - session actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  #helpers personalisados
+  helper_method :firefighter_session?, :get_firefighter_session
+
+  #adicionar usuário na sessão
   def add_firefighter_session(registration)
   	session[:current_firefighter_registration] = registration
   end
+
+  #buscar usuário da sessão
+  def get_firefighter_session
+  	if Firefighter.exists?(:registration => session[:current_firefighter_registration])
+  		return Firefighter.find_by registration: session[:current_firefighter_registration]
+  	else
+  		return nil
+  	end
+  end
+
+  #verificar presença de usuário na sessão
+  def firefighter_session?
+  	session[:current_firefighter_registration].present?
+  end
+
+  #destruir sessão
+  def destroy_firefighter_session
+  	session[:current_firefighter_registration] = nil
+  end
+
 end
