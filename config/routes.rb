2872f55fbb9a464dfcfc45913a8fecf107734e34
Asus-Linux <asus-linux@localhost.localdomain>
2018-03-09 17:41:06 -0300

    add route district

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,11 @@ Rails.application.routes.draw do
 
   get 'logout', to: 'auth#logout'
 
+
+  # ROTAS ANINHADAS
+  resources :districts do
+    resources :adresses
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
