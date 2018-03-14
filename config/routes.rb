f9fbd172aff78f5bb669f15a6acf470b5a03f921
Asus-Linux <asus-linux@localhost.localdomain>
2018-03-14 15:28:32 -0300

    add route type_of_cars and link type_of_cars in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,9 @@ Rails.application.routes.draw do
     resources :adresses
   end
 
+  resources :type_of_cars do
+    resources :cars
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
