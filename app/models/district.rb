af3576436ff3b7ddfaaad174bb8a9ddc0fac0873
Asus-Linux <asus-linux@localhost.localdomain>
2018-03-14 14:37:46 -0300

    displaying errors districts

--- app/models/district.rb
+++ app/models/district.rb
@@ -2,4 +2,7 @@ class District < ApplicationRecord
 
 	has_many :addresses, dependent: :destroy
 
+	validates_presence_of :name, message: 'O campo nome não pode ser deixado em branco'
+	validates_uniqueness_of :name, message: 'Bairro já cadastrado'
+
 end
