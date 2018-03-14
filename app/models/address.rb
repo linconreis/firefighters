690c6de489f53d82f6f18f700a577fe44cfaa913
Asus-Linux <asus-linux@localhost.localdomain>
2018-03-14 14:36:52 -0300

    displaying errors adresses

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,8 @@ class Address < ApplicationRecord
 belongs_to :district
 has_many :event_logs
 
+
+validates_presence_of :name, message: 'O campo nome não pode ser deixado em branco'
+validates_uniqueness_of :name, message: 'Rua já cadastrada'
+
 end
