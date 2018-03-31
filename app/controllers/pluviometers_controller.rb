7ac58f34ff6a26a2e93cbc8abe393d7cdfa2b443
Asus-Linux <asus-linux@localhost.localdomain>
2018-03-31 18:03:01 -0300

    CRUD - pluviomenters more displaying errors

---
+++ app/controllers/pluviometers_controller.rb
@@ -0,0 +1,54 @@
+class PluviometersController < ApplicationController
+
+	def index
+		@pluviometers = Pluviometer.order(:date).all
+	end
+
+	def new
+		@pluviometer = Pluviometer.new
+	end
+
+	def create
+		@pluviometer = Pluviometer.new(pluviometer_params)
+		@pluviometer.firefighter = get_firefighter_session
+
+			if @pluviometer.save
+				redirect_to controller: "pluviometers"
+				flash[:success] = "Cadastrado com sucesso"
+			else
+				render :new
+				flash[:danger] = "Opss! Algo deu errado"
+			end
+	end
+
+	def edit
+		@pluviometer = Pluviometer.find(params[:id])
+	end
+
+	def update
+		@pluviometer = Pluviometer.find(params[:id])
+		@pluviometer.firefighter = get_firefighter_session
+
+		if @pluviometer.update(pluviometer_params)
+			redirect_to controller: "pluviometers"
+			flash[:success] = "Atualizado com sucesso"
+		else
+			render :edit
+			flash[:danger] = "Atualizado com sucesso"
+			
+		end
+	end
+
+	def destroy
+		@pluviometer = Pluviometer.find(params[:id])
+		@pluviometer.destroy
+		redirect_to controller: "pluviometers"
+		flash[:success] = "Excluído com sucesso"
+	end
+
+	private
+		def pluviometer_params
+			params.require(:pluviometer).permit(:last_twenty_four, :last_forty_eight, :date, :note)
+		end
+
+end
---
+++ app/helpers/pluviometers_helper.rb
@@ -0,0 +1,2 @@
+module PluviometersHelper
+end
