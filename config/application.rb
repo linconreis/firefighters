72d6399ec77a48a7713a6bad64fe592552de26fc
HP-LINUX <hp-linux@localhost.localdomain>
2018-04-22 22:05:27 -0300

    add javascript - jquery

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,10 @@ module Firefighters
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # java script - jquery
+    config.generators do |g|
+      g.javascript_engine :js
+    end
   end
 end
