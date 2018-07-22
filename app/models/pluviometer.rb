531addf4095a82c0297d16fc6eddb442b5efadbb
HP-LINUX <hp-linux@localhost.localdomain>
2018-07-22 13:54:27 -0300

    date event uniqueness - pluviometers

--- app/models/pluviometer.rb
+++ app/models/pluviometer.rb
@@ -6,5 +6,7 @@ class Pluviometer < ApplicationRecord
   validates_presence_of :last_twenty_four, message: 'O campo chuva das últ. 24hrs não pode ser deixado em branco'
   validates_presence_of :last_forty_eight, message: 'O campo chuva das últ. 48hrs não pode ser deixado em branco'
 
+  validates_uniqueness_of :date, message: 'Data do evento já cadastrada'
+
 
 end
