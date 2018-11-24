cae5856f96e1002786de5ac4f67492c82d2afa1a
HP-LINUX <hp-linux@localhost.localdomain>
2018-11-24 17:49:58 -0200

    print button - show event_logs

--- app/assets/javascripts/event_logs.js
+++ app/assets/javascripts/event_logs.js
@@ -1,2 +1,21 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+;(function () {
+
+  'use strict';
+
+
+  $('#btn-imprimir-pedido').click(function () {
+    var conteudo = document.getElementById('print1').innerHTML;
+    var conteudo2 = document.getElementById('print2').innerHTML,
+      tela_impressao = window.open('about:blank');
+
+      tela_impressao.document.write(conteudo + conteudo2);
+      tela_impressao.window.print();
+      tela_impressao.window.close();
+  });
+
+
+})();
+m
